e80c8d92a8030a51f07978c3fdfc57a786ae1916
Mike Huynh <mhuynh517@gmail.com>
2018-05-21 16:55:15 -0700

    changing from reverse a string to caesars ciper

--- caesarscipher.js
+++ caesarscipher.js
@@ -1,3 +1,22 @@
-function reverseString(){
-    console.log();
+function rot13(str) { 
+  let mystr = "";
+  
+
+
+  let i = 0; 
+    while(i < str.length){
+      if(str.charCodeAt([i]) <= 63) {
+        mystr += String.fromCharCode(str.charCodeAt([i]));
+      } else if(str.charCodeAt([i]) >= 78) {
+          mystr += String.fromCharCode(str.charCodeAt([i])- 13);
+          }
+          else if (str.charCodeAt([i]) >= 65) { 
+            mystr += String.fromCharCode(str.charCodeAt([i]) + 13); 
+          }
+      i++;
+    }
+return mystr;
+
 }
+
+rot13("SERR YBIR!");
