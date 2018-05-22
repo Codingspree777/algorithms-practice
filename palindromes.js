0926dd7eeac1b0b40b835a6c35fd17c6b40b2967
Mike Huynh <mhuynh517@gmail.com>
2018-05-22 15:51:58 -0700

    Correct spelling and input code to solve algorithm

---
+++ palindromes.js
@@ -0,0 +1,15 @@
+function palindromes(str) {
+  let cleanstrings = str.replace(/[^0-9a-z]/gi, '');
+  let lowerstr = cleanstrings.toLowerCase();
+  let mystr = "";
+  console.log(cleanstrings);
+  
+    for(let i=lowerstr.length-1; i>=0; i--) { 
+      mystr += lowerstr[i];
+    }
+    if(mystr === lowerstr) {
+      return true;
+    } else if (mystr !== lowerstr) { 
+      return false;
+    }
+}
