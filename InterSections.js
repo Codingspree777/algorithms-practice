cff61339058f2efc39aa02f4c47f625b87c01c95
Mike Huynh <mhuynh517@gmail.com>
2018-06-01 14:37:06 -0700

    use a for-loop and objects to solve algo

--- InterSections.js
+++ InterSections.js
@@ -1,5 +1,26 @@
 function interSection(arrayOfArrays){
-
+    let newarr = [];
+    const modeObj = {};
+  
+    for(let i = 0; i < arrayOfArrays.length; i++){
+      let subarr = arrayOfArrays[i];
+      for(let j = 0; j < subarr.length; j++){
+        if(modeObj[subarr[j]] === undefined) {
+        modeObj[subarr[j]] = 0;
+        } 
+        modeObj[subarr[j]] += 1;
+      }
+    }
+     console.log(modeObj)
+     let track = arrayOfArrays.length
+     
+    for(let prop in modeObj) {
+      if(modeObj[prop] === track) {
+        newarr.push(parseInt(prop));
+      }
+    }
+    return newarr
+  }
 
 }
 
