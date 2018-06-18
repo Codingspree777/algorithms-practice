eb0c331c776f10c85c47d60de5651f3556bf544b
Mike Huynh <mhuynh517@gmail.com>
2018-06-17 22:03:56 -0700

    use reduce and obj to solve algo

--- InterSections.js
+++ InterSections.js
@@ -25,32 +25,28 @@
 
   /// for-loop
   function intersection(arrayOfArrays) {
-    let newarr = []
-    let auxarr = []
+    let newarr = [];
   
-    for(let j = 0; j < arrayOfArrays.length; j++){
-      let arr1 = arrayOfArrays[0]
-      let arr2 = arrayOfArrays[1]
-    }
+    const buildObj = arrayOfArrays.reduce((myObj, arr) => { const subarr = arr.reduce((key, value, i)=>{
+          if(myObj[value] === undefined) {
+          myObj[value] = 0;
+          } 
+          myObj[value] += 1;
+        },0)
+        return myObj
+      }, {});
+      
+    let track = arrayOfArrays.length
   
-    for(let i = 0; i < arr1.length; i++) {
-      if(arr2.indexOf(arr1[i]) !== -1){
-        auxarr.push(arr1[i])
+    for(let prop in buildObj) {
+      if(buildObj[prop] === track) {
+        newarr.push(parseInt(prop));
       }
     }
-  
-    let joined = [].concat(...arrayOfArrays)
-  
-    for(let k = 0; k < auxarr.length; k++){
-      if(joined.indexOf(auxarr[k]) !== -1) {
-        newarr.push(auxarr[k])
-      }
-    }
-  
-  
-  return newarr;
-  
+    return newarr
   }
+  
+
 
 
 intersection([[5, 10, 15, 20], [15, 88, 1, 5, 7], [1,10,15,5,20]]);
